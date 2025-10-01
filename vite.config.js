5972c1b3c60e1e48ffa9ec8d3481e624f33522dc
Brad Snider <bsnider@arborday.org>
2025-10-01 14:04:45 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // If deploying to https://<user>.github.io/<repo>/ set base to '/<repo>/'
 // For a user/organization root site (repo named <user>.github.io) leave base as '/'
-const repoName = "havasupai"; // change if your repo name differs
+const repoName = "havasupai-react"; // change if your repo name differs
 const isRootSite = false; // set true if publishing to <user>.github.io
 
 export default defineConfig({
